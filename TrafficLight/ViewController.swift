ffd451fa7f978170d1800ca4360362acd95fd9dd
Aleksandr Kryukov <kryukov86@mail.ru>
2021-08-18 22:28:37 +0700

    Add Outlets and Action

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    @IBOutlet weak var changeColorButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+        
+        redLightView.layer.cornerRadius = redLightView.bounds.width/2
+        yellowLightView.layer.cornerRadius = redLightView.bounds.width/2
+        greenLightView.layer.cornerRadius = redLightView.bounds.width/2
+        
+        changeColorButton.layer.cornerRadius = 10
+        
     }
 
-
+    @IBAction func buttonPressed() {
+    }
+    
 }
 
